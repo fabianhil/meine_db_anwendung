d6f6d8bd76d24938830551b5592a0e3dc56fde85
fabian <fabian.hilbich@stud.thga.de>
2022-11-28 22:10:06 +0100

    Add CLI11

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <CLI/CLI.hpp>
 
-int main()
+int main(int argc, char** argv)
 {
 	std::cout << "Hello World" << "\n";
 
+	CLI::App app{"Dies ist ein Programm was uns den Umgang mit Lagerhaltungsdaten zeigen soll","Lagerhaltungsprogramm"};
+
+	 try{
+        app.parse(argc, argv);
+    } catch(const CLI::ParseError& e){
+        return app.exit(e);
+    }
 }
