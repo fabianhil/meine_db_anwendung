516aa31bdb0650f2b7812b198e1029d79683c75d
fabian <fabian.hilbich@stud.thga.de>
2022-12-12 14:39:28 +0100

    file einlesen und ausgeben

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <CLI/CLI.hpp>
+#include <string>
+#include <fstream>
+#include <nlohmann/json.hpp>
 
 int main(int argc, char** argv)
 {
@@ -7,9 +10,27 @@ int main(int argc, char** argv)
 
 	CLI::App app{"Dies ist ein Programm was uns den Umgang mit Lagerhaltungsdaten zeigen soll","Lagerhaltungsprogramm"};
 
+    std::string filepath;
+    app.add_option("-r,--read", filepath,"Path to config file")
+        ->required()
+        ->check(CLI::ExistingFile);
+        
 	 try{
         app.parse(argc, argv);
     } catch(const CLI::ParseError& e){
         return app.exit(e);
     }
+
+    std::ifstream file{filepath};
+    if(!file.is_open()){
+        std::cout << "Error opening file!\n";
+        exit(0);
+    }
+
+    nlohmann::json FileToJson;
+    file >> FileToJson;
+
+    std::cout << FileToJson.dump() << "\n";
+
+    return 0;
 }
